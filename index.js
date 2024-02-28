5b89b44c0f88e6b4c777293c5e0bf0db19383452
Luieitalian <enderbud125@hotmail.com>
2024-02-28 05:06:49 +0300

    organise: App structure

--- index.js
+++ index.js
@@ -1,9 +1,32 @@
-/**
- * @format
- */
-
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
+import {MD3DarkTheme, MD3LightTheme, PaperProvider} from 'react-native-paper';
+import LightTheme from './themes/LightTheme.json';
+import DarkTheme from './themes/DarkTheme.json';
+import {SafeAreaProvider} from 'react-native-safe-area-context';
+import {useColorScheme} from 'react-native';
 
-AppRegistry.registerComponent(appName, () => App);
+const theme = {
+  dark: {
+    ...MD3DarkTheme,
+    colors: DarkTheme.colors,
+  },
+  light: {
+    ...MD3LightTheme,
+    colors: LightTheme.colors,
+  },
+};
+
+const AppMiddleWare = () => {
+  const isDarkMode = useColorScheme() === 'dark';
+  return (
+    <PaperProvider theme={isDarkMode ? theme.dark : theme.light}>
+        <SafeAreaProvider>
+          <App />
+        </SafeAreaProvider>
+    </PaperProvider>
+  );
+};
+
+AppRegistry.registerComponent(appName, () => AppMiddleWare);
