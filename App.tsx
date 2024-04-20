a5019706a0a7c3d2aaeddbaedf3f4188f7dd16c2
Luieitalian <enderbud125@hotmail.com>
2024-04-21 02:17:10 +0300

    add: i18next integration

--- App.tsx
+++ App.tsx
@@ -3,17 +3,23 @@ import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import {NavigationContainer} from '@react-navigation/native';
 import LoginScreen from './screens/LoginScreen';
 import ProductsScreen from './screens/ProductsScreen';
-
+import {useTranslation} from 'react-i18next';
 const Stack = createNativeStackNavigator();
 
 function App(): React.JSX.Element {
+  const {t, i18n} = useTranslation();
+
   return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="Login">
-        <Stack.Screen name="Login" component={LoginScreen} />
-        <Stack.Screen name="Products" component={ProductsScreen} />
-      </Stack.Navigator>
-    </NavigationContainer>
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName="Login">
+          <Stack.Screen
+            name="Login Screen"
+            options={{headerShown: false}}
+            component={LoginScreen}
+          />
+          <Stack.Screen name="Products" component={ProductsScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
   );
 }
 
---
+++ i18n.js
@@ -0,0 +1,22 @@
+import i18n from 'i18next';
+import {initReactI18next} from 'react-i18next';
+
+import enTranslation from './locales/en.json';
+import trTranslation from './locales/tr.json';
+
+i18n
+  .use(initReactI18next)
+  .init({
+    compatibilityJSON: 'v3',
+    debug: true,
+    fallbackLng: 'en',
+    supportedLngs: ['en', 'tr'],
+    resources: {
+      en: {
+        translation: enTranslation,
+      },
+      tr: {
+        translation: trTranslation,
+      },
+    },
+  });
