5d824eb59d809c8a24822584d87c2790b53ad37c
Luieitalian <enderbud125@hotmail.com>
2024-04-24 03:58:21 +0300

    add: implement getting version data from mock db, style and password input

--- screens/LoginScreen.tsx
+++ screens/LoginScreen.tsx
@@ -1,26 +1,45 @@
-import React, {useContext, useEffect, useState} from 'react';
+import React, {useContext, useEffect, useRef, useState} from 'react';
 import {
   Text,
   StyleSheet,
   View,
   Pressable,
   StatusBar,
-  useColorScheme,
-  KeyboardAvoidingView,
+  TextInput,
 } from 'react-native';
-import {useTheme, Icon, Menu, Divider, TextInput} from 'react-native-paper';
-import {useTranslation, withTranslation} from 'react-i18next';
+import {useTheme, Icon, Menu, Divider} from 'react-native-paper';
+import {useTranslation} from 'react-i18next';
 import {changeLanguage} from 'i18next';
 import {SafeAreaView} from 'react-native-safe-area-context';
 import {ThemeContext} from '../contexts/ThemeContext';
+import axios from 'axios';
 
 const LoginScreen = ({route, navigation}: any) => {
   const theme = useTheme();
   const {t, i18n} = useTranslation();
   const [visibleLangMenu, setVisibleLangMenu] = useState(false);
+  const [version, setVersion] = useState<string>('[unknown]');
+  const pwdRef = useRef<TextInput>(null); // A Ref for password textinput component
+
+  useEffect(() => {
+    const getVersion = () => {
+      axios
+        .get('http://10.0.2.2:3000/version') // 10.0.0.2 Special alias to the host loopback interface
+        .then((res) => {
+          setVersion(res.data);
+        })
+        .catch((e) => {
+          console.log(e);
+        });
+    };
+    getVersion();
+  }, []);
 
   const closeLangMenu = () => setVisibleLangMenu(false);
   const openLangMenu = () => setVisibleLangMenu(true);
+  const focusOnPwd = () => {
+    pwdRef.current?.focus();
+  };
 
   const {toggleTheme} = useContext(ThemeContext);
 
@@ -37,30 +56,33 @@ const LoginScreen = ({route, navigation}: any) => {
         wrapper: {
           flex: 1,
           marginTop: 24,
+          marginBottom: 12,
+          justifyContent: 'space-between',
+          alignItems: 'center',
           backgroundColor: theme.colors.onSecondaryContainer,
           borderRadius: 12,
+          padding: 40,
+          paddingBottom: 10,
+          width: '80%',
+        },
+        contentWrapper: {
+          flex: 0.6,
+          alignItems: 'center',
+          justifyContent: 'space-around',
         },
         welcome: {
-          flex: 1,
-          justifyContent: 'center',
-          alignItems: 'center',
           fontSize: 40,
+          fontFamily: 'Roboto-Light',
+          textAlign: 'center',
           color: theme.colors.background,
         },
-        welcomeView: {
-          flex: 1,
-          padding: 24,
-          flexDirection: 'column',
-          justifyContent: 'center',
-          alignItems: 'center',
-        },
         header: {
-          flex: 0.07,
+          flex: 0.08,
           alignItems: 'center',
           flexDirection: 'row',
           justifyContent: 'flex-end',
           width: '100%',
-          gap: 10,
+          gap: 20,
           paddingHorizontal: 20,
           backgroundColor: theme.colors.surfaceVariant,
         },
@@ -86,10 +108,22 @@ const LoginScreen = ({route, navigation}: any) => {
           color: theme.colors.background,
         },
         form: {
-          flex: 1,
+          gap: 10,
         },
         textInput: {
+          minWidth: '100%',
           backgroundColor: theme.colors.surfaceVariant,
+          borderRadius: 10,
+          color: theme.colors.onSurfaceVariant,
+          paddingHorizontal: 15,
+        },
+        footer: {
+          justifyContent: 'center',
+          alignItems: 'center',
+        },
+        version: {
+          color: theme.colors.surfaceVariant,
+          fontFamily: 'Roboto-Medium',
         },
       }),
     [theme]
@@ -148,18 +182,32 @@ const LoginScreen = ({route, navigation}: any) => {
       </View>
       <Divider style={styles.divider} theme={theme} bold={true} />
       <View style={styles.wrapper}>
-        <View style={styles.welcomeView}>
-          <Text style={styles.welcome}>{t('welcome_to_app')}</Text>
+        <View style={styles.contentWrapper}>
+          <View>
+            <Text style={styles.welcome}>{t('welcome_to_app')}</Text>
+          </View>
+          <View style={styles.form}>
+            <TextInput
+              placeholderTextColor={theme.colors.onSurfaceVariant}
+              autoCorrect={false}
+              inputMode="text"
+              onSubmitEditing={focusOnPwd}
+              style={styles.textInput}
+              placeholder={t('username')}
+            />
+            <TextInput
+              ref={pwdRef}
+              secureTextEntry={true}
+              placeholderTextColor={theme.colors.onSurfaceVariant}
+              autoCorrect={false}
+              inputMode="text"
+              style={styles.textInput}
+              placeholder={t('password')}
+            />
+          </View>
         </View>
-        <View style={styles.form}>
-          <TextInput
-            style={styles.textInput}
-            outlineColor={theme.colors.onSurfaceVariant}
-            activeOutlineColor={theme.colors.onSurfaceVariant}
-            textColor={theme.colors.primary}
-            mode="outlined"
-            label={t('username')}
-          />
+        <View style={styles.footer}>
+          <Text style={styles.version}>{`${t('version')} ${version}`}</Text>
         </View>
       </View>
     </SafeAreaView>
