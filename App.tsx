aa36d4bc18ed0307bec8d83f7db855dbb52247a4
Luieitalian <enderbud125@hotmail.com>
2024-04-25 00:07:32 +0300

    remove: translation imports as they are unnecessary

--- App.tsx
+++ App.tsx
@@ -1,25 +1,27 @@
 import React from 'react';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
-import {NavigationContainer} from '@react-navigation/native';
+import {NavigationContainer, useTheme} from '@react-navigation/native';
 import LoginScreen from './screens/LoginScreen';
 import ProductsScreen from './screens/ProductsScreen';
-import {useTranslation} from 'react-i18next';
+
 const Stack = createNativeStackNavigator();
 
 function App(): React.JSX.Element {
-  const {t, i18n} = useTranslation();
-
   return (
-      <NavigationContainer>
-        <Stack.Navigator initialRouteName="Login">
-          <Stack.Screen
-            name="Login Screen"
-            options={{headerShown: false}}
-            component={LoginScreen}
-          />
-          <Stack.Screen name="Products" component={ProductsScreen} />
-        </Stack.Navigator>
-      </NavigationContainer>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="LoginScreen">
+        <Stack.Screen
+          name="LoginScreen"
+          options={{headerShown: false}}
+          component={LoginScreen}
+        />
+        <Stack.Screen
+          name="Products"
+          options={{headerShown: false}}
+          component={ProductsScreen}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
