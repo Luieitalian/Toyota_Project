572431aa4b38a3150b0ebdc1469fb5797cc8c4eb
Luieitalian <enderbud125@hotmail.com>
2024-04-24 07:37:46 +0300

    add: a custom hook to use NFC

---
+++ hooks/useNFC.tsx
@@ -0,0 +1,23 @@
+import NfcManager, {NfcTech} from 'react-native-nfc-manager';
+
+NfcManager.start();
+
+const useNFC = () => {
+  async function readNdef() {
+    try {
+      // register for the NFC tag with NDEF in it
+      await NfcManager.requestTechnology(NfcTech.Ndef);
+      // the resolved tag object will contain `ndefMessage` property
+      const tag = await NfcManager.getTag();
+      console.warn('Tag found', tag);
+    } catch (ex) {
+      console.warn('Oops!', ex);
+    } finally {
+      // stop the nfc scanning
+      NfcManager.cancelTechnologyRequest();
+    }
+  }
+  return readNdef;
+};
+
+export default useNFC;
