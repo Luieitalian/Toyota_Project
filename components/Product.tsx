1d89f22be6ffc39e00378ad4ebeac20424d817bb
Luieitalian <enderbud125@hotmail.com>
2024-04-27 15:42:52 +0300

    chore: wrap component with <Suspense>

--- components/Product.tsx
+++ components/Product.tsx
@@ -1,10 +1,11 @@
-import React, {memo} from 'react';
+import React, {memo, Suspense} from 'react';
 import {Image, Text, View} from 'react-native';
 import useProductStyle from './styles/useProductStyle';
 import {TFunction} from 'i18next';
 import {ActivityIndicator, MD3Theme} from 'react-native-paper';
 import {ProductModel} from '../models/ProductModel';
 import useProductImage from '../hooks/useProductImage';
+import ProductLoading from './ProductLoading';
 
 type ProductProps = {
   t: TFunction<'translation', undefined>;
@@ -17,16 +18,18 @@ const Product = ({t, theme, prod}: ProductProps) => {
   const {imageURL, loadingImageURL} = useProductImage(prod);
 
   return (
-    <View style={styles.productContainer}>
-      {loadingImageURL ? (
-        <ActivityIndicator theme={theme} />
-      ) : (
-        <Image style={styles.image} source={{uri: imageURL}} />
-      )}
-      <Text style={styles.price}>{prod.price}</Text>
-      <Text style={styles.name}>{prod.name}</Text>
-      <Text style={styles.amount_attribute}>{prod.amount_attribute}</Text>
-    </View>
+    <Suspense fallback={<ProductLoading theme={theme} />}>
+      <View style={styles.productContainer}>
+        {loadingImageURL ? (
+          <ActivityIndicator theme={theme} />
+        ) : (
+          <Image style={styles.image} source={{uri: imageURL}} />
+        )}
+        <Text style={styles.price}>{prod.price}</Text>
+        <Text style={styles.name}>{prod.name}</Text>
+        <Text style={styles.amount_attribute}>{prod.amount_attribute}</Text>
+      </View>
+    </Suspense>
   );
 };
 
