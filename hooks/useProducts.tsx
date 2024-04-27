03379d47404cdac4ca24875f599fbbf8fcd59671
Luieitalian <enderbud125@hotmail.com>
2024-04-27 04:48:55 +0300

    revamp: hook to be able to filter out products based on optional arguments

--- hooks/useProducts.tsx
+++ hooks/useProducts.tsx
@@ -1,20 +1,42 @@
-import React, {useEffect, useState} from 'react';
+import {useEffect, useState} from 'react';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import axios from 'axios';
-import dev_local_products from '../db/all_products.json';
+import dev_local_products from '../db/db.json';
+import {ProductModel} from '../models/ProductModel';
 
-const useProducts = (isOnline: boolean) => {
-  const [products, setProducts] = useState<string>('[]');
+const useProducts = (
+  isOnline: boolean,
+  optionalSearchText?: string,
+  optionalCategory?: string | undefined
+) => {
+  const [products, setProducts] = useState<ProductModel[]>([]);
+  const [loadingProducts, setLoadingProducts] = useState<boolean>(true);
 
   useEffect(() => {
+    setLoadingProducts(true);
     if (isOnline) {
       const getProductsFromServer = async () => {
         await axios
-          .get('http://10.0.2.2:3001/products') // 10.0.2.2 Special alias to the host loopback interface
+          .get('http://192.168.0.101:3000/products') // 10.0.2.2 Special alias to the host loopback interface
           .then((res) => {
-            const data = JSON.stringify(res.data);
             console.log('Getting products from server!');
-            setProducts(data);
+
+            let filteredProds = res.data as ProductModel[];
+
+            if (optionalSearchText !== undefined) {
+              filteredProds = filteredProds.filter((prod: ProductModel) =>
+                prod.name.toLocaleLowerCase('tr').includes(optionalSearchText)
+              );
+            }
+
+            if (optionalCategory !== undefined) {
+              filteredProds = filteredProds.filter(
+                (prod: ProductModel) => prod.category === optionalCategory
+              );
+            }
+
+            setProducts(filteredProds);
+            setLoadingProducts(false);
           })
           .catch((error) => {
             console.log(error);
@@ -24,8 +46,8 @@ const useProducts = (isOnline: boolean) => {
     } else {
       const getProductsFromLocalDB = async () => {
         try {
-          let data = await AsyncStorage.getItem('products');
-          if (data === null) {
+          let local_data_exists = await AsyncStorage.getItem('products');
+          if (local_data_exists === null) {
             console.log('Local products is empty!');
             console.log('Adding products to async storage from local server!');
             await AsyncStorage.setItem(
@@ -33,9 +55,26 @@ const useProducts = (isOnline: boolean) => {
               JSON.stringify(dev_local_products.products)
             );
           }
-          data = await AsyncStorage.getItem('products');
           console.log('Getting products from local storage!');
-          setProducts(data as string);
+          const local_data_string = await AsyncStorage.getItem('products');
+
+          let filteredProds = JSON.parse(
+            local_data_string as string
+          ) as ProductModel[];
+
+          if (optionalSearchText !== undefined) {
+            filteredProds = filteredProds.filter((prod: ProductModel) =>
+              prod.name.toLocaleLowerCase('tr').includes(optionalSearchText)
+            );
+          }
+          if (optionalCategory !== undefined) {
+            filteredProds = filteredProds.filter(
+              (prod: ProductModel) => prod.category === optionalCategory
+            );
+          }
+
+          setProducts(filteredProds);
+          setLoadingProducts(false);
         } catch (error) {
           console.log(error);
         }
@@ -43,9 +82,9 @@ const useProducts = (isOnline: boolean) => {
       getProductsFromLocalDB();
     }
     return () => console.log('returning from effect');
-  }, []);
+  }, [optionalSearchText, optionalCategory]);
 
-  return products;
+  return {products, loadingProducts};
 };
 
 export default useProducts;
