f6610414b8955b839f4d3768f5fa3c66c7c57eb5
Luieitalian <enderbud125@hotmail.com>
2024-04-27 04:47:43 +0300

    chore: fix styling

--- screens/styles/useProductsScreenStyle.tsx
+++ screens/styles/useProductsScreenStyle.tsx
@@ -7,18 +7,42 @@ const useProductsScreenStyle = () => {
   const styles = React.useMemo(
     () =>
       StyleSheet.create({
-        view: {
+        screenView: {
           flex: 1,
           flexDirection: 'column',
           alignItems: 'center',
+          justifyContent: 'flex-start',
+          paddingTop: 5,
           gap: 5,
-          backgroundColor: theme.colors.surfaceVariant,
+          backgroundColor: theme.colors.background,
+        },
+        wrapper: {
+          flex: 1,
+          flexDirection: 'row',
+          gap: 5,
         },
         filtersContainer: {
           flex: 1,
         },
         productsContainer: {
           flex: 1,
+        },
+        listHeader: {
+          flex: 0.2,
+          width: '100%',
+          alignItems: 'flex-start',
+          justifyContent: 'center',
+          paddingHorizontal: 14,
+        },
+        flatlistContainer: {
+          flex: 1,
+        },
+        flatlist: {
+          gap: 10,
+          justifyContent: 'center',
+          alignItems: 'center',
+          marginTop: 10,
+          paddingBottom: 10,
         },
       }),
     [theme]
