380ce9d74bf33edd6483c500945e6cf9fd554054
Luieitalian <enderbud125@hotmail.com>
2024-05-17 00:11:25 +0300

    feat: add clear cart button

---
+++ components/ClearCart.tsx
@@ -0,0 +1,30 @@
+import {TFunction} from 'i18next';
+import React, {memo, useContext, useState} from 'react';
+import {MD3Theme, Button} from 'react-native-paper';
+import CustomButton from './CustomButton';
+import useClearCartStyle from './styles/useClearCartStyle';
+import {ShoppingCartContext} from '../contexts/ShoppingCartContext';
+
+type ClearCartProps = {
+  t: TFunction<'translation', undefined>;
+  theme: MD3Theme;
+};
+
+const ClearCart = ({t, theme}: ClearCartProps) => {
+  const {styles} = useClearCartStyle(theme);
+
+  const {clearCart} = useContext(ShoppingCartContext);
+
+  const onPress = () => {
+    console.log('Clearing Cart!');
+    clearCart();
+  };
+
+  return (
+    <CustomButton theme={theme} styles={styles} onPress={onPress}>
+      {t('clear_cart')}
+    </CustomButton>
+  );
+};
+
+export default memo(ClearCart);
---
+++ components/styles/useClearCartStyle.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import {StyleSheet, useWindowDimensions} from 'react-native';
+import {MD3Theme} from 'react-native-paper';
+
+const useClearCartStyle = (theme: MD3Theme) => {
+  const styles = React.useMemo(() => {
+    return StyleSheet.create({
+      container: {},
+      text: {},
+    });
+  }, [theme]);
+
+  return {styles};
+};
+
+export default useClearCartStyle;
