bb796eeefb65f33cb3f0dcb1ecf49f95d7f71aee
Luieitalian <enderbud125@hotmail.com>
2024-05-18 14:40:12 +0300

    feat: implement lookupprice button

--- components/LookUpPrice.tsx
+++ components/LookUpPrice.tsx
@@ -1,9 +1,13 @@
 import {TFunction} from 'i18next';
 import React, {memo, useContext, useState} from 'react';
 import {Pressable, Text, View} from 'react-native';
-import {MD3Theme} from 'react-native-paper';
+import {Dialog, MD3Theme, Modal, Portal, TextInput} from 'react-native-paper';
 import useLookUpPriceStyle from './styles/useLookUpPriceStyle';
 import CustomButton from './CustomButton';
+import Product from './Product';
+import {ShoppingCartContext} from '../contexts/ShoppingCartContext';
+import {ProductsContext} from '../contexts/ProductsContext';
+import {ProductModel} from '../models/ProductModel';
 
 type LookUpPriceProps = {
   t: TFunction<'translation', undefined>;
@@ -12,15 +16,74 @@ type LookUpPriceProps = {
 
 const LookUpPrice = ({t, theme}: LookUpPriceProps) => {
   const {styles} = useLookUpPriceStyle(theme);
+  const [modalVisible, setModalVisible] = useState<boolean>(false);
+  const [IDText, setIDText] = useState<string | undefined>(undefined);
+  const [productShown, setProductShown] = useState<ProductModel | undefined>(
+    undefined
+  );
 
-  const onPress = () => {
-    console.log('look up price');
+  const {addToCart} = useContext(ShoppingCartContext);
+  const {products} = useContext(ProductsContext);
+
+  const onPress = () => {};
+
+  const showModal = () => {
+    setModalVisible(true);
+  };
+
+  const hideModal = () => {
+    setModalVisible(false);
+  };
+
+  const onChangeText = (text: string) => {
+    setIDText(text);
+  };
+
+  const onSubmitEditing = () => {
+    const product = products.filter(
+      (prod: ProductModel) => prod.id === IDText
+    )[0];
+    setProductShown(product);
+    if (product !== undefined) {
+    }
   };
 
   return (
-    <CustomButton theme={theme} styles={styles} onPress={onPress}>
-      {t('look_up_price')}
-    </CustomButton>
+    <>
+      <Portal>
+        <Modal
+          visible={modalVisible}
+          onDismiss={hideModal}
+          contentContainerStyle={styles.modal}
+        >
+          <TextInput
+            inputMode="numeric"
+            onSubmitEditing={onSubmitEditing}
+            style={styles.textInput}
+            placeholder={t('press_enter_id')}
+            value={IDText}
+            onChangeText={onChangeText}
+          />
+          {productShown !== undefined && modalVisible ? (
+            <Product
+              prod={productShown}
+              addToCart={addToCart}
+              t={t}
+              theme={theme}
+            />
+          ) : (
+            <View>
+              <Text style={styles.warningMessage}>
+                {t('please_enter_a_valid_id')}
+              </Text>
+            </View>
+          )}
+        </Modal>
+      </Portal>
+      <CustomButton theme={theme} styles={styles} onPress={showModal}>
+        {t('look_up_price')}
+      </CustomButton>
+    </>
   );
 };
 
--- components/styles/useLookUpPriceStyle.tsx
+++ components/styles/useLookUpPriceStyle.tsx
@@ -11,6 +11,27 @@ const useLookUpPriceStyle = (theme: MD3Theme) => {
     return StyleSheet.create({
       container: {},
       text: {},
+      modal: {
+        flex: 1,
+        flexDirection: 'column',
+        justifyContent: 'flex-start',
+        alignItems: 'center',
+        gap: 20,
+        marginHorizontal: isWide ? 400 : 80,
+        marginVertical: isWide ? 200 : 200,
+        padding: 20,
+        backgroundColor: theme.colors.background,
+        borderWidth: 2,
+        borderRadius: 10,
+        borderColor: theme.colors.primaryContainer,
+      },
+      textInput: {
+        width: '100%',
+      },
+      warningMessage: {
+        color: theme.colors.onBackground,
+        fontSize: isWide ? 20 : 16,
+      },
     });
   }, [theme, width]);
   return {styles};
