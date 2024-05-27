d2fa6c0510c8ccb95634deb3890bc623932cfd33
Luieitalian <enderbud125@hotmail.com>
2024-05-27 20:12:20 +0300

    feat: add footer component & styles, add some locales

---
+++ components/Footer.tsx
@@ -0,0 +1,43 @@
+import React, {useContext} from 'react';
+import {Text, View} from 'react-native';
+import useFooterStyle from './styles/useFooterStyle';
+import {TFunction} from 'i18next';
+import {Button, Icon, MD3Theme, Menu} from 'react-native-paper';
+import {StatusContext} from '../contexts/StatusContext';
+
+type FooterProps = {
+  t: TFunction<'translation', undefined>;
+  theme: MD3Theme;
+};
+
+const Footer = ({t, theme}: FooterProps) => {
+  const {isOnline, toggleOnlineStatus} = useContext(StatusContext);
+
+  const {styles} = useFooterStyle({theme, isOnline});
+
+  const onPress = () => {
+    toggleOnlineStatus();
+  };
+
+  return (
+    <View style={styles.footerContainer}>
+      <View style={styles.menuContainer}>
+        <Button onPress={onPress} compact mode="contained">
+          {t('debug_toggle_online_status')}
+        </Button>
+      </View>
+      <View style={styles.shopStatusContainer}>
+        <Text style={styles.text}>
+          {t('shop_is_online', {status: isOnline ? t('online') : t('offline')})}
+        </Text>
+        <Icon
+          color={styles.icon.color}
+          size={styles.icon.width}
+          source="circle"
+        />
+      </View>
+    </View>
+  );
+};
+
+export default Footer;
