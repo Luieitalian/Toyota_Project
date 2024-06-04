992f3253973dfb6f735faf2bee4b1744485bc499
Luieitalian <enderbud125@hotmail.com>
2024-06-04 03:42:41 +0300

    chore: implement SafeAreaProviderWrapper

--- contexts/SafeAreaProviderWrapper.tsx
+++ contexts/SafeAreaProviderWrapper.tsx
@@ -1,5 +1,6 @@
 import React, {memo} from 'react';
 import {StyleSheet} from 'react-native';
+import {useTheme} from 'react-native-paper';
 import {SafeAreaProvider} from 'react-native-safe-area-context';
 
 type SafeAreaProviderWrapperProps = {
@@ -7,16 +8,22 @@ type SafeAreaProviderWrapperProps = {
 };
 
 const SafeAreaProviderWrapper = ({children}: SafeAreaProviderWrapperProps) => {
+  const theme = useTheme();
+
   const styles = React.useMemo(
     () =>
       StyleSheet.create({
-        sap: {
+        SafeAreaProvider: {
           backgroundColor: theme.colors.background,
         },
       }),
-    []
+    [theme]
   );
-  return <SafeAreaProvider style={styles.sap}></SafeAreaProvider>;
+  return (
+    <SafeAreaProvider style={styles.SafeAreaProvider}>
+      {children}
+    </SafeAreaProvider>
+  );
 };
 
 export default memo(SafeAreaProviderWrapper);
