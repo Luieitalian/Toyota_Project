551805bc60c71adc08af59ed29183aea086c6dbc
Luieitalian <enderbud125@hotmail.com>
2024-06-10 22:53:23 +0300

    chore: move code to a seperate file

--- components/FilteringBar.tsx
+++ components/FilteringBar.tsx
@@ -3,61 +3,21 @@ import {ScrollView, View} from 'react-native';
 import useFilteringBarStyle from './styles/useFilteringBarStyle';
 import {SegmentedButtons, useTheme} from 'react-native-paper';
 import {useTranslation} from 'react-i18next';
-
-const ButtonCategories = [
-  'show_all',
-  'favorites',
-  'et_balik',
-  'dondurma',
-  'ev_yasam',
-  'firindan',
-  'fit_form',
-  'kahvalti',
-  'meyve_sebze',
-  'su_icecek',
-  'sut_urunleri',
-  'teknoloji',
-];
-
-const ButtonCategoryIcons: {[char: string]: string} = {
-  show_all: 'view-headline',
-  favorites: 'star',
-  et_balik: 'food-drumstick',
-  dondurma: 'ice-cream',
-  ev_yasam: 'home',
-  firindan: 'baguette',
-  fit_form: 'basketball',
-  kahvalti: 'egg-fried',
-  meyve_sebze: 'fruit-watermelon',
-  su_icecek: 'liquor',
-  sut_urunleri: 'cow',
-  teknoloji: 'lightning-bolt',
-};
+import {Categories, CategoryIcons} from '../globals/categories';
 
 type FilteringBarProps = {
   category: string | undefined;
-  disabled?: boolean;
   onChangeCategory: (category: string) => void;
 };
 
-const FilteringBar = ({
-  category,
-  disabled = false,
-  onChangeCategory,
-}: FilteringBarProps) => {
+const FilteringBar = ({category, onChangeCategory}: FilteringBarProps) => {
   const theme = useTheme();
   const {t} = useTranslation();
 
   const {styles} = useFilteringBarStyle(theme);
-  const [visibleCategoryMenu, setVisibleCategoryMenu] =
-    useState<boolean>(false);
-
-  const closeCategoryMenu = () => setVisibleCategoryMenu(false);
-  const openCategoryMenu = () => setVisibleCategoryMenu(true);
 
   const onChangeCategoryMiddleWare = (category: string) => {
     onChangeCategory(category);
-    closeCategoryMenu();
   };
 
   return (
@@ -68,10 +28,10 @@ const FilteringBar = ({
           onValueChange={onChangeCategoryMiddleWare}
           value={category ? category : 'favorites'}
           buttons={[
-            ...ButtonCategories.map((cat: string) => ({
+            ...Categories.map((cat: string) => ({
               value: cat,
               label: t(cat),
-              icon: ButtonCategoryIcons[cat],
+              icon: CategoryIcons[cat],
               style: {height: 40},
             })),
           ]}
---
+++ globals/categories.ts
@@ -0,0 +1,29 @@
+export const Categories = [
+  'show_all',
+  'favorites',
+  'et_balik',
+  'dondurma',
+  'ev_yasam',
+  'firindan',
+  'fit_form',
+  'kahvalti',
+  'meyve_sebze',
+  'su_icecek',
+  'sut_urunleri',
+  'teknoloji',
+];
+
+export const CategoryIcons: {[char: string]: string} = {
+  show_all: 'view-headline',
+  favorites: 'star',
+  et_balik: 'food-drumstick',
+  dondurma: 'ice-cream',
+  ev_yasam: 'home',
+  firindan: 'baguette',
+  fit_form: 'basketball',
+  kahvalti: 'egg-fried',
+  meyve_sebze: 'fruit-watermelon',
+  su_icecek: 'liquor',
+  sut_urunleri: 'cow',
+  teknoloji: 'lightning-bolt',
+};
