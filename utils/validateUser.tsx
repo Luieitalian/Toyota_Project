d8829c332f23341d7e8f31a3750972ac1aa35695
Luieitalian <enderbud125@hotmail.com>
2024-07-05 00:51:21 +0300

    feat: add biometrics

--- utils/validateUser.tsx
+++ utils/validateUser.tsx
@@ -1,5 +1,34 @@
 import {UserModel} from '@/models/UserModel';
 
+import ReactNativeBiometrics, {BiometryTypes} from 'react-native-biometrics';
+
+const rnBiometrics = new ReactNativeBiometrics();
+let _publicKey: string;
+
+rnBiometrics.isSensorAvailable().then((resultObject) => {
+  const {available} = resultObject;
+
+  if (available) {
+    rnBiometrics.createKeys().then((resultObject) => {
+      const {publicKey} = resultObject;
+      console.log(publicKey);
+      _publicKey = publicKey;
+    });
+    rnBiometrics
+      .createSignature({
+        promptMessage: 'Sign in',
+        payload: _publicKey,
+      })
+      .then((resultObject) => {
+        const {success, signature} = resultObject;
+
+        if (success) {
+          console.log(signature);
+        }
+      });
+  }
+});
+
 const validateUser = (
   username: string,
   password: string,
